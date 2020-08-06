31beaca2445c67242b6c623698b6deded498eba1
rzhygit <rusya.liamg@gmail.com>
2020-08-06 04:10:51 +0300

    make GitHub pages working

---
+++ build/precache-manifest.3f36539964c7ffb729be0174ac89a8dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c84494c5a3ca11f71a535d76795fec4",
+    "url": "/intetics-testtask/index.html"
+  },
+  {
+    "revision": "aa8dbe662ef60d8d9b1a",
+    "url": "/intetics-testtask/static/css/main.5cc2fa68.chunk.css"
+  },
+  {
+    "revision": "514e361cb6f774e3e577",
+    "url": "/intetics-testtask/static/js/2.387ce8a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/intetics-testtask/static/js/2.387ce8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8dbe662ef60d8d9b1a",
+    "url": "/intetics-testtask/static/js/main.650c40c9.chunk.js"
+  },
+  {
+    "revision": "3ba222428d502ab0cd9b",
+    "url": "/intetics-testtask/static/js/runtime-main.7fc524e5.js"
+  }
+]);
